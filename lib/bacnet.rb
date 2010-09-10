9d4a6565d4000229afa7641aafadfdcd2ab8d90d
Scott Shanks <Scott.Shanks@emerson.com>
2010-09-10 15:17:27 -0400

    Initial file - no methods defined

---
+++ lib/bacnet.rb
@@ -0,0 +1,26 @@
+
+=begin rdoc
+*Revisions*
+  | Initial File                              | Scott Shanks        | 9/10/2010|
+
+*Module_Name*
+  Bacnet
+
+*Description*
+  Bacnet methods - This is a wrapper for the bacnetconsole application created
+by Chad Mrazek
+
+http://126.4.1.113/twiki/bin/view/LmgEmbedded/MAT_BACnet_Client_Command_Line.
+
+*Variables*
+
+=end
+
+module  Bacnet
+  PATH_TO_BACNETCONSOLE = "C:\\Program Files\\Bacnet\\Console\\bacnetConsole.exe"
+
+  def
+  end
+
+end
+
